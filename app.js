1b2339750f51ab6eb49b448a741ee2b01fd857eb
kuzin455 <kuzin455@gmail.com>
2021-05-24 21:06:30 +0300

    first attempts to work with git

---
+++ app.js
@@ -0,0 +1,22 @@
+$(function () {
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+
+  let scrollPos = $(window).scrollTop();
+
+  $(window).on("scroll load resize", function () {
+    let introH = intro.innerHeight();
+
+    scrollPos = $(this).scrollTop();
+
+    if (scrollPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+    console.log(scrollPos);
+  });
+
+  console.log(scrollPos);
+});
